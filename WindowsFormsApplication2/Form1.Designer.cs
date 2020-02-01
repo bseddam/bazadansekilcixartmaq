657b3820232e49e0f747375e96f8c0be5c65e82c
seddam bayramov <bseddam@gmail.com>
2020-02-01 17:14:49 +0400

    ilk fayl

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsekilexport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnsekilexport
+            // 
+            this.btnsekilexport.Location = new System.Drawing.Point(292, 63);
+            this.btnsekilexport.Name = "btnsekilexport";
+            this.btnsekilexport.Size = new System.Drawing.Size(75, 23);
+            this.btnsekilexport.TabIndex = 2;
+            this.btnsekilexport.Text = "sekil export";
+            this.btnsekilexport.UseVisualStyleBackColor = true;
+            this.btnsekilexport.Click += new System.EventHandler(this.btnsekilexport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(428, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "msg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 452);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnsekilexport);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnsekilexport;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void btnsekilexport_Click(object sender, EventArgs e)
+        {
+            Class2 klas = new Class2();
+            var exportPath = @"D:\namizedwekil";
+            if (!Directory.Exists(exportPath))
+            {
+                Directory.CreateDirectory(exportPath);
+            }
+            DataTable imagesdatatable = klas.getdatatable(@"SELECT m.[dsk_kodu]
+      ,[soyadi]
+      ,[adi]
+      ,[ata_adi]
+      ,[FOTO] 
+FROM [MM2020].[dbo].[Namized_MM] as m 
+inner join MM2020.dbo.NamizedSenedMM as n on n.namID= m.Table_id 
+where [FOTO] is not null 
+and cix_tarix is null and qeyd_alinm is not null order by m.dsk_kodu,soyadi
+      ,adi
+      ,ata_adi");
+
+            foreach (DataRow row in imagesdatatable.Rows)
+            {
+                if (row.ItemArray.Length > 0)
+                {
+                    if (row[0] != null)
+                    {
+                        byte[] image1 = (byte[])row["FOTO"];
+                        Image newImage1 = convertbinarytoimage(image1);
+                        var fileName1 = string.Format(row["dsk_kodu"].ToString() + "."
+                                       + row["adi"].ToString() + row["soyadi"].ToString() +
+                                       row["ata_adi"].ToString() + ".jpg");
+                        using (var image = newImage1)
+                        using (var newImage = ScaleImage(image, 180, 241))
+                        {
+                            newImage.Save(@"d:\namizedwekil\" + fileName1, ImageFormat.Jpeg);
+                        }
+                    }
+                }
+            }
+            label1.Text = "hazirdi";
+        }
+        public static Image ScaleImage(Image image, int maxWidth, int maxHeight)
+        {
+            var newImage = new Bitmap(maxWidth, maxHeight);
+            using (var graphics = Graphics.FromImage(newImage))
+                graphics.DrawImage(image, 0, 0, maxWidth, maxHeight);
+            return newImage;
+        }
+        Image convertbinarytoimage(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                return Image.FromStream(ms);
+            }
+        }
+    }
+}
